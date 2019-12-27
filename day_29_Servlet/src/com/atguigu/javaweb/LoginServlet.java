c549c2fa47f4d47114bd5474f26412e01a33cb0d
wojiaochadi <264835229@qq.com>
2019-12-27 11:04:40 +0800

    ServletRequest对象

--- day_29_Servlet/src/com/atguigu/javaweb/LoginServlet.java
+++ day_29_Servlet/src/com/atguigu/javaweb/LoginServlet.java
@@ -5,7 +5,10 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
 
 public class LoginServlet implements Servlet {
 
@@ -22,6 +25,29 @@ public class LoginServlet implements Servlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("请求进来了....");
+
+        //根据请求参数的名字，返回参数值
+        String user = servletRequest.getParameter("user");
+        //根据请求参数的名字，返回请求参数的一组的字符串数组
+        String[] parameterValues = servletRequest.getParameterValues("password");
+        //返回所有请求参数的键值对
+        Map<String, String[]> parameterMap = servletRequest.getParameterMap();
+        //返回所有请求参数的名字
+        Enumeration<String> parameterNames = servletRequest.getParameterNames();
+
+        //获取HttpServletRequest请求对象
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        //获取请求的URL      http://localhost:8080/day29/loginServlet
+        System.out.println(httpServletRequest.getRequestURL());
+        //获取请求的URI      /day29/loginServlet
+        System.out.println(httpServletRequest.getRequestURI());
+        //获取请求的方式        GET
+        System.out.println(httpServletRequest.getMethod());
+        //获取请求的参数String     user=admin&password=admin
+        System.out.println(httpServletRequest.getQueryString());
+        //获取请求的servlet名字    /loginServlet
+        System.out.println(httpServletRequest.getServletPath());
+
 
     }
 
