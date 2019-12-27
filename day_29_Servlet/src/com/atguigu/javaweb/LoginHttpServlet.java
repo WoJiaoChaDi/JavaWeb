a0487e463f7905f6fe3e9c8f5b33129741d1030c
wojiaochadi <264835229@qq.com>
2019-12-27 14:53:17 +0800

    继承HttpServlet，实现 doGet  doPost  方法

---
+++ day_29_Servlet/src/com/atguigu/javaweb/LoginHttpServlet.java
@@ -0,0 +1,25 @@
+package com.atguigu.javaweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginHttpServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String method = req.getMethod();
+        System.out.println(method);
+
+        StringBuffer requestURL = req.getRequestURL();
+        System.out.println(requestURL);
+
+        String user = req.getParameter("user");
+        System.out.println("user:" + user);
+
+        super.doPost(req, resp);
+    }
+}
