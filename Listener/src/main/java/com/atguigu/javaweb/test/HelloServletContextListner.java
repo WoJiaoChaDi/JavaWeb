1fcf243d8e1e4630f8f780c4780e3466742023b0
wojiaochadi <264835229@qq.com>
2020-01-07 16:28:40 +0800

    ServletContextListener ServletRequestListener ServletSessionListener

--- Listener/src/main/java/com/atguigu/javaweb/test/HelloServletContextListner.java
+++ Listener/src/main/java/com/atguigu/javaweb/test/HelloServletContextListner.java
@@ -2,9 +2,13 @@ package com.atguigu.javaweb.test;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 public class HelloServletContextListner
-		implements ServletContextListener {
+		implements ServletContextListener, ServletRequestListener, HttpSessionListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
@@ -14,6 +18,29 @@ public class HelloServletContextListner
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		System.out.println("ServletContext 对象被销毁。" + sce.getServletContext());
+	}
+
+	@Override
+	public void sessionCreated(HttpSessionEvent se) {
+		System.out.println("HttpSession 被创建。");
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent se) {
+		// TODO Auto-generated method stub
+		System.out.println("HttpSession 被销毁");
+	}
+
+	@Override
+	public void requestDestroyed(ServletRequestEvent sre) {
+		// TODO Auto-generated method stub
+		System.out.println("ServletRequest 被销毁");
+	}
+
+	@Override
+	public void requestInitialized(ServletRequestEvent sre) {
+		// TODO Auto-generated method stub
+		System.out.println("ServletRequest 被创建");
 	}
 
 }
